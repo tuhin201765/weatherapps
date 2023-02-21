ac6bd9357b83dcbc75e3502481cb0b2e04e27baf
tuhin201765 <tuhin201765@gmail.com>
2023-02-21 21:43:36 +0600

    working code

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
+# here we are import path from in-built django-urls
 from django.urls import path
-from.import views
+
+# here we are importing all the Views from the views.py file
+from . import views
 
 urlpatterns = [
-    path('', views.index, name= 'index')
+    path('', views.index, name='home'),
 ]
